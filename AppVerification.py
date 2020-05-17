bbdd029bb1be515e226640f531d63e090ffbeceb
Mohammed Rashid Chowdhury <rashid0531@gmail.com>
2020-05-18 05:23:47 +0600

    Removed commented out code blocks and unnecessary functions.

--- AppVerification.py
+++ AppVerification.py
@@ -1,12 +1,10 @@
 from collections import defaultdict
-
 import Application_setup as appsetup
 import Utilities as util
 import abc
 
 
 class AppVerification(metaclass=abc.ABCMeta):
-
     @abc.abstractmethod
     def verify_parameters(self, args: dict):
         pass
@@ -18,7 +16,6 @@ class AppVerification(metaclass=abc.ABCMeta):
 
 
 class ParamVerificationSinglePicture(AppVerification):
-
     def __init__(self, args: dict):
         self.task_params = defaultdict(lambda: None)
         self.args = args
@@ -45,7 +42,7 @@ class ParamVerificationSinglePicture(AppVerification):
                         self.task_params['tile_size'] = tile_size
 
                     if not util.check_if_dir_exists(self.args['save_path']):
-                        self.task_params['save_path'] = (True, self.args['save_path'])
+                        self.task_params['save_path'] = self.args['save_path']
 
                     remaining_actions_to_add = self.actions - self.task_params.keys()
                     for each_action in remaining_actions_to_add:
@@ -63,34 +60,11 @@ class ParamVerificationSinglePicture(AppVerification):
                 print(f"Invalid actions : {err_str}.")
 
             self.task_params['task'] = self.args['task']
-
-            # all_param_verified = []
-            # for action in self.actions:
-            #     if self.task_params[action] is None:
-            #         all_param_verified.append(0)
-            #     else:
-            #         all_param_verified.append(1)
             all_param_verified = [0 if self.task_params[action] is None else 1 for action in self.actions]
             return all(all_param_verified)
 
-    @classmethod
-    def prepare_coordinates(cls, coors: list):
-        coordinates = []
-        try:
-            for each in coors:
-                cor_name, cor_values = each
-                cor_values = util.format_string_to_int(cor_values)
-                if len(cor_values) != constants.ELEMENTS_NEEDED_TO_DEFINE_COORDINATES:
-                    raise Exception(f"There needs to be two elements to define a coordinate. Recheck {cor_name}")
-                    continue
-                cor_object = CoOrdinate(cor_name, cor_values)
-                coordinates.append(cor_object)
-        except Exception as e:
-            print(e)
-
 
 class ParamVerificationMultiplePictures(AppVerification):
-
     def __init__(self, args: dict):
         self.settings = None
         self.args = args
