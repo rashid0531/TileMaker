87c88fdc75f6f73dd76eb1e94e2641e8ea807f7f
Mohammed Rashid Chowdhury <rashid0531@gmail.com>
2020-05-21 23:03:29 +0600

    Added abstracts of parameter verification methods for each task.

--- verify_param.py
+++ verify_param.py
@@ -1,25 +1,33 @@
-class VerifyParameters(object):
+import Application_setup as appsetup
+import abc
+
+
+class VerifyParameters:
     @classmethod
-    def verify(cls, args: dict):
-        coordinates = []
-        for key, value in args.items():
-            if 'cor' in str(key):
-                coordinates.append((key, args[key]))
+    def create_appropriate_app_verification(cls, args: dict):
+        try:
+            if args['task'] in appsetup.TASKS:
+                task_name = args['task']
+                verification_method = appsetup.TASKS[task_name](args)
+                verified = verification_method.verify_parameters()
+                if verified:
+                    return verified, verification_method.settings()
+                else:
+                    return verified, None
             else:
-                img_path = args['image_path']
-                tile_size = args['tile_size']
-                save_path = args['save_path']
-
-        prepare_coordinates(coordinates)
-
-    def prepare_coordinates(coors):
-
-
+                # TODO
+                # Check if this block of code executes or not.
+                task_list = ''
+                for each in appsetup.TASKS.keys():
+                    task_list += '-' + each + '\n'
+                print(f'Invalid Task. Possible tasks are: \n \t {task_list}')
+        except KeyError as e:
+            print(f' {e} Not found.')
 
 
 
 class CoOrdinate(object):
 
-    def __init__(self, name, coor:tuple):
+    def __init__(self, name, coor: list):
         self.name = name
         self.abscissa, self.ordinate = coor
